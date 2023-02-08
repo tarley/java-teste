48a8f994e2966438687219b89a664ea2bf7a40f7
Tarley Lana <tarley.lana@gmail.com>
2023-02-07 22:00:06 -0300

    feat: adiciona a biblioteca lombok.

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
 
         var usuario = new Usuario();
+        usuario.getNome();
     }
 }
--- src/br/newtonpaiva/Usuario.java
+++ src/br/newtonpaiva/Usuario.java
@@ -1,22 +1,9 @@
 package br.newtonpaiva;
 
+import lombok.Getter;
+
+@Getter
 public class Usuario {
     private String nome;
     private Long idade;
-
-    public Long getIdade() {
-        return idade;
-    }
-
-    public void setIdade(Long idade) {
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
