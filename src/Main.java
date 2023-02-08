15a295c2727e127e8268e476c07255e9004ec6f5
Tarley Lana <tarley.lana@gmail.com>
2023-02-07 21:32:39 -0300

    teste

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import br.newtonpaiva.Usuario;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var usuario = new Usuario();
+    }
+}
